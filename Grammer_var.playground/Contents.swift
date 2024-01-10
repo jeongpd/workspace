8307c81f32d97dd3de195db9f81abb109feea763
Tiger[ 정덕범 ] <db.jeong@linecorp.com>
2024-01-11 00:15:57 +0900

    1st day study

---
+++ Grammer_var.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+var name: String = "Tiger"
+var age: Int = 20
+
+name
+name = "타이거"
+
+age
+age = 42
+
+
+let one: int = 1
+let two: int = 2
+
+func sayHello(home: String) -> {
+    return "Nice to meet you, \(name)"
+}
